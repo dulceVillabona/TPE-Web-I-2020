26c907a5e02fc195bdb6fb4b72ea609f4b5dc627
dulylula <45400886+dulylula@users.noreply.github.com>
2020-05-31 18:09:39 -0300

    Responsive (#7)
    * Header, Home y Footer responsive. Inserte form dinamico para agregar cervezas a la tabla
    
    * Datos precargados en forma de json en el arreglo cervezas
    
    Co-authored-by: nanomiguez <zaseznano@gmail.com>

---
+++ js/tabla.js
@@ -0,0 +1,163 @@
+"use strict"
+
+let cervezas = [
+    {
+        "nombre": "American IPA",
+        "img": "src",
+        "alcohol": "5.5-7.5 %",
+        "IBU": "40.0-65.0",
+        "OG": "1.056-1.075",
+        "FG": "1.010-1.018"
+    },{
+        "nombre": "Belgian Stout",
+        "img": "src",
+        "alcohol": "6.0-7.5 %",
+        "IBU": "20.0-30.0",
+        "OG": "1.062-1.075",
+        "FG": "1.008-1.016"
+    },{
+        "nombre": "Golden Ale",
+        "img": "src",
+        "alcohol": "4.2-5.0 %",
+        "IBU": "20.0-25.0",
+        "OG": "1.041-1.050",
+        "FG": "1.009-1.018"
+    },{
+        "nombre": "Honey",
+        "img": "src",
+        "alcohol": "4.5-5.8 %",
+        "IBU": "30.0-35.0",
+        "OG": "1.050-1.060",
+        "FG": "1.005-1.015"
+    },{
+        "nombre": "Irish Red",
+        "img": "src",
+        "alcohol": "8.0–12.0 %",
+        "IBU": "50.0–85.0",
+        "OG": "1.075 – 1.115",
+        "FG": "1.018 – 1.030"
+    },{
+        "nombre": "Kölsch",
+        "img": "src",
+        "alcohol": "3.5–5.0 %",
+        "IBU": "18.0-30.0",
+        "OG": "1.044–1.050",
+        "FG": "1.007–1.011"
+    }
+];
+
+
+
+
+
+document.getElementById("sumar-button").addEventListener("click", agregar_1_cerveza);
+document.getElementById("restar-button").addEventListener("click", restar_1_cerveza);
+document.getElementById("sumar-bucle-button").addEventListener("click", agregar_3_cervezas);
+document.getElementById("reset-button").addEventListener("click", resetear_cervezas_agregadas);
+
+
+
+
+function agregar_1_cerveza() {
+
+    /* -----------------------------OBTENGO VALORES DEL FORM------------------------- */
+
+    let nombre_cerveza = document.getElementById("beer-name").value;
+    let imagen_cerveza = document.getElementById("beer-image").value;
+    let alcohol_cerveza = document.getElementById("beer-alcohol").value;
+    let IBU_cerveza = document.getElementById("beer-IBU").value;
+    let OG_cerveza = document.getElementById("beer-OG").value;
+    let FG_cerveza = document.getElementById("beer-FG").value;
+
+/* ----------------------------------CRÉO EL JSON "CERVEZA"-------------------- */
+
+    let cerveza = {
+        "nombre": nombre_cerveza,
+        "img": imagen_cerveza,
+        "alcohol": alcohol_cerveza,
+        "IBU": IBU_cerveza,
+        "OG": OG_cerveza,
+        "FG": FG_cerveza
+    }
+    
+
+/* -----------------------------------PUSHEO LA CERVEZA AL ARRAY DE CERVEZAS------------------- */
+
+    cervezas.push(cerveza);
+    console.log(cervezas[cervezas.length - 1].IBU);
+
+/* -----------------------------------CRÉO EL HTML Y SU CONTENIDO------------------- */
+
+    let crearFila = document.createElement("TR");
+    let crearCelda1 = document.createElement("TD");
+    let textoCelda1 = document.createTextNode(cervezas[cervezas.length - 1].nombre);
+    let crearCelda2 = document.createElement("TD");
+    let textoCelda2 = document.createTextNode(cervezas[cervezas.length -1].img);
+    let crearCelda3 = document.createElement("TD");
+    let textoCelda3 = document.createTextNode(cervezas[cervezas.length - 1].alcohol);
+    let crearCelda4 = document.createElement("TD");
+    let textoCelda4 = document.createTextNode(cervezas[cervezas.length - 1].IBU);
+    let crearCelda5 = document.createElement("TD");
+    let textoCelda5 = document.createTextNode(cervezas[cervezas.length - 1].OG);
+    let crearCelda6 = document.createElement("TD");
+    let textoCelda6 = document.createTextNode(cervezas[cervezas.length - 1].FG);
+
+/* -------------------------------------ÚNO EL CONTENIDO AL HTML----------------- */
+
+    crearCelda1.appendChild(textoCelda1);
+    crearCelda2.appendChild(textoCelda2);
+    crearCelda3.appendChild(textoCelda3);
+    crearCelda4.appendChild(textoCelda4);
+    crearCelda5.appendChild(textoCelda5);
+    crearCelda6.appendChild(textoCelda6);
+    
+/* -------------------------------------ÚNO TODOS LOS "TD" AL "TR"----------------- */
+
+    crearFila.appendChild(crearCelda1);
+    crearFila.appendChild(crearCelda2);
+    crearFila.appendChild(crearCelda3);
+    crearFila.appendChild(crearCelda4);
+    crearFila.appendChild(crearCelda5);
+    crearFila.appendChild(crearCelda6);
+
+/* -------------------------------------INSERTO EL "TR" AL HTML DE LA TABLA
+                                                Y LE AGREGO UN ID-------------------- */
+
+    crearFila.setAttribute("id", cervezas.length)
+    document.getElementById("tabla-body").appendChild(crearFila);
+
+
+
+}
+
+function restar_1_cerveza() {
+
+/* -------------------------------------SELECCIONO AL ULTIMO "TR"----------------- */
+
+    let ultimoTR = document.getElementById(cervezas.length);
+    
+/* -------------------------------------ELIMINO EL "TR" Y LA CERVEZA----------------- */
+    
+    ultimoTR.remove();
+    cervezas.pop();
+    
+}
+
+function agregar_3_cervezas() {
+
+    let bucle_number = document.getElementById("bucle-number").value;
+
+    for (let i = 0; i < bucle_number; i++) {
+        agregar_1_cerveza();
+        console.log(cervezas[i]);
+        console.log(cervezas);
+    }
+}
+
+function resetear_cervezas_agregadas() {
+
+    for (let i = cervezas.lenght; i != 0; i--) {
+        restar_1_cerveza();
+    }
+
+}
