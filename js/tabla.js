fa4a9ec6e9e7ea7d7511df489efa5f4940b535e0
dulylula <45400886+dulylula@users.noreply.github.com>
2020-06-07 13:54:55 -0300

    Responsive y filtro (#8)
    * Header, Home y Footer responsive. Inserte form dinamico para agregar cervezas a la tabla
    
    * Datos precargados en forma de json en el arreglo cervezas
    
    * Tabla dinamica mejorada, sin datos en el html, resetear resetea toda la tabla incluyendo datos precargados
    
    * tabla y contacto responsive
    
    * js refactor
    
    * Cambio cargarTabla() y arreglo el resetear_cervezas_agregadas
    
    * Cambie el cargarTabla() y le agregue imagenes al form de la tabla
    
    * responsive y filtro
    
    * merge css
    
    * merge css
    
    Co-authored-by: nanomiguez <zaseznano@gmail.com>

--- js/tabla.js
+++ js/tabla.js
@@ -1,163 +1,183 @@
-"use strict"
+("use strict");
+
+document.addEventListener("DOMContentLoaded", cargarTabla);
 
 let cervezas = [
-    {
-        "nombre": "American IPA",
-        "img": "src",
-        "alcohol": "5.5-7.5 %",
-        "IBU": "40.0-65.0",
-        "OG": "1.056-1.075",
-        "FG": "1.010-1.018"
-    },{
-        "nombre": "Belgian Stout",
-        "img": "src",
-        "alcohol": "6.0-7.5 %",
-        "IBU": "20.0-30.0",
-        "OG": "1.062-1.075",
-        "FG": "1.008-1.016"
-    },{
-        "nombre": "Golden Ale",
-        "img": "src",
-        "alcohol": "4.2-5.0 %",
-        "IBU": "20.0-25.0",
-        "OG": "1.041-1.050",
-        "FG": "1.009-1.018"
-    },{
-        "nombre": "Honey",
-        "img": "src",
-        "alcohol": "4.5-5.8 %",
-        "IBU": "30.0-35.0",
-        "OG": "1.050-1.060",
-        "FG": "1.005-1.015"
-    },{
-        "nombre": "Irish Red",
-        "img": "src",
-        "alcohol": "8.0–12.0 %",
-        "IBU": "50.0–85.0",
-        "OG": "1.075 – 1.115",
-        "FG": "1.018 – 1.030"
-    },{
-        "nombre": "Kölsch",
-        "img": "src",
-        "alcohol": "3.5–5.0 %",
-        "IBU": "18.0-30.0",
-        "OG": "1.044–1.050",
-        "FG": "1.007–1.011"
-    }
+  {
+    name: "American Ipa",
+    imageSrc: "../img/American_IPA.png",
+    alcohol: "5.5-7.5 %",
+    ibu: "40.0-65.0",
+    og: "1.056-1.075",
+    fg: "1.010-1.018",
+    sinStock: false,
+  },
+  {
+    name: "Belgian Stout",
+    imageSrc: "../img/Belgian-Stout.png",
+    alcohol: "6.0-7.5 %",
+    ibu: "20.0-30.0",
+    og: "1.062-1.075",
+    fg: "1.008-1.016",
+    sinStock: false,
+  },
+  {
+    name: "Golden Ale",
+    imageSrc: "../img/Golden_Ale.png",
+    alcohol: "4.2-5.0 %",
+    ibu: "20.0-25.0",
+    og: "1.041-1.050",
+    fg: "1.009-1.018",
+    sinStock: true,
+  },
+  {
+    name: "Honey",
+    imageSrc: "../img/Honey.png",
+    alcohol: "4.5-5.8 %",
+    ibu: "30.0-35.0",
+    og: "1.050-1.060",
+    fg: "1.005-1.015",
+    sinStock: true,
+  },
+  {
+    name: "Irish Red",
+    imageSrc: "../img/Irish-Red.png",
+    alcohol: "8.0–12.0 %",
+    ibu: "50.0–85.0",
+    og: "1.075 – 1.11",
+    fg: "1.018 – 1.030",
+    sinStock: false,
+  },
+  {
+    name: "Kölsch",
+    imageSrc: "../img/Kolsch.png",
+    alcohol: "3.5–5.0 %",
+    ibu: "18.0-30.0",
+    og: "1.044–1.050",
+    fg: "1.007–1.011",
+    sinStock: true,
+  },
 ];
 
+function cargarTabla() {
+  let mensaje = document.getElementById("sin-cervezas");
+  let table = document.querySelector("#tabla-comparacion");
+  let switchDiv = document.getElementById("stock-switch");
+  table.innerHTML = "";
+  mensaje.innerHTML = "";
+  switchDiv.classList.remove("ocultar");
+  console.log(cervezas)
+  if (cervezas.length > 0) {
+    let crearThead = document.createElement("THEAD");
+    let crearTR = document.createElement("TR");
 
+    let titulosThead = ["Cerveza", "Color", "Alcohol", "IBU", "OG", "FG"];
 
-
-
-document.getElementById("sumar-button").addEventListener("click", agregar_1_cerveza);
-document.getElementById("restar-button").addEventListener("click", restar_1_cerveza);
-document.getElementById("sumar-bucle-button").addEventListener("click", agregar_3_cervezas);
-document.getElementById("reset-button").addEventListener("click", resetear_cervezas_agregadas);
-
-
-
-
-function agregar_1_cerveza() {
-
-    /* -----------------------------OBTENGO VALORES DEL FORM------------------------- */
-
-    let nombre_cerveza = document.getElementById("beer-name").value;
-    let imagen_cerveza = document.getElementById("beer-image").value;
-    let alcohol_cerveza = document.getElementById("beer-alcohol").value;
-    let IBU_cerveza = document.getElementById("beer-IBU").value;
-    let OG_cerveza = document.getElementById("beer-OG").value;
-    let FG_cerveza = document.getElementById("beer-FG").value;
-
-/* ----------------------------------CRÉO EL JSON "CERVEZA"-------------------- */
-
-    let cerveza = {
-        "nombre": nombre_cerveza,
-        "img": imagen_cerveza,
-        "alcohol": alcohol_cerveza,
-        "IBU": IBU_cerveza,
-        "OG": OG_cerveza,
-        "FG": FG_cerveza
+    for (let i = 0; i < titulosThead.length; i++) {
+      let nuevoTh = document.createElement("TH");
+      nuevoTh.innerHTML = titulosThead[i];
+      crearTR.appendChild(nuevoTh);
     }
-    
 
-/* -----------------------------------PUSHEO LA CERVEZA AL ARRAY DE CERVEZAS------------------- */
+    crearThead.appendChild(crearTR);
 
-    cervezas.push(cerveza);
-    console.log(cervezas[cervezas.length - 1].IBU);
+    table.appendChild(crearThead);
 
-/* -----------------------------------CRÉO EL HTML Y SU CONTENIDO------------------- */
+    let tableBody = table.createTBody();
+    tableBody.id = "tabla-body";
 
-    let crearFila = document.createElement("TR");
-    let crearCelda1 = document.createElement("TD");
-    let textoCelda1 = document.createTextNode(cervezas[cervezas.length - 1].nombre);
-    let crearCelda2 = document.createElement("TD");
-    let textoCelda2 = document.createTextNode(cervezas[cervezas.length -1].img);
-    let crearCelda3 = document.createElement("TD");
-    let textoCelda3 = document.createTextNode(cervezas[cervezas.length - 1].alcohol);
-    let crearCelda4 = document.createElement("TD");
-    let textoCelda4 = document.createTextNode(cervezas[cervezas.length - 1].IBU);
-    let crearCelda5 = document.createElement("TD");
-    let textoCelda5 = document.createTextNode(cervezas[cervezas.length - 1].OG);
-    let crearCelda6 = document.createElement("TD");
-    let textoCelda6 = document.createTextNode(cervezas[cervezas.length - 1].FG);
-
-/* -------------------------------------ÚNO EL CONTENIDO AL HTML----------------- */
-
-    crearCelda1.appendChild(textoCelda1);
-    crearCelda2.appendChild(textoCelda2);
-    crearCelda3.appendChild(textoCelda3);
-    crearCelda4.appendChild(textoCelda4);
-    crearCelda5.appendChild(textoCelda5);
-    crearCelda6.appendChild(textoCelda6);
-    
-/* -------------------------------------ÚNO TODOS LOS "TD" AL "TR"----------------- */
-
-    crearFila.appendChild(crearCelda1);
-    crearFila.appendChild(crearCelda2);
-    crearFila.appendChild(crearCelda3);
-    crearFila.appendChild(crearCelda4);
-    crearFila.appendChild(crearCelda5);
-    crearFila.appendChild(crearCelda6);
-
-/* -------------------------------------INSERTO EL "TR" AL HTML DE LA TABLA
-                                                Y LE AGREGO UN ID-------------------- */
-
-    crearFila.setAttribute("id", cervezas.length)
-    document.getElementById("tabla-body").appendChild(crearFila);
-
-
-
+    for (let i = 0; i < cervezas.length; i++) {
+      let row = tableBody.insertRow();
+      if (cervezas[i].sinStock === false) {
+        row.classList.add("en_stock");
+      }
+      for (key in cervezas[i]) {
+        if (key !== "sinStock") {
+          let cell = row.insertCell();
+          if (key === "imageSrc") {
+            cell.innerHTML = `<img src=${cervezas[i][key]} alt=${cervezas[i].name} />`;
+          } else {
+            cell.innerHTML = cervezas[i][key];
+          }
+        }
+      }
+    }
+  } else {
+    mensaje.innerHTML = "No hay cervezas cargadas";
+    switchDiv.classList.add("ocultar");
+  }
 }
 
+let limpiarForm = () => {
+  let form = document.querySelector("#tabla-comparacion");
+  let inputs = form.querySelectorAll("input");
+  for (let i = 0; i < inputs.length; i++) {
+    inputs[i].value = "";
+  }
+};
+
+let agregar_1_cerveza = () => {
+  event.preventDefault();
+
+  let nuevaCerveza = {
+    name: document.getElementById("beer-name").value,
+    imageSrc: document.querySelector('input[name="cervezaImg"]:checked').value,
+    alcohol: document.getElementById("beer-alcohol").value,
+    ibu: document.getElementById("beer-IBU").value,
+    og: document.getElementById("beer-OG").value,
+    fg: document.getElementById("beer-FG").value,
+    sinStock: document.getElementById("sin-stock").checked,
+  };
+  cervezas.push(nuevaCerveza);
+  cargarTabla();
+  console.log(nuevaCerveza);
+};
+
 function restar_1_cerveza() {
-
-/* -------------------------------------SELECCIONO AL ULTIMO "TR"----------------- */
-
-    let ultimoTR = document.getElementById(cervezas.length);
-    
-/* -------------------------------------ELIMINO EL "TR" Y LA CERVEZA----------------- */
-    
-    ultimoTR.remove();
-    cervezas.pop();
-    
+  cervezas.pop();
+  cargarTabla();
 }
 
 function agregar_3_cervezas() {
+  let bucle_number = document.getElementById("bucle-number").value;
 
-    let bucle_number = document.getElementById("bucle-number").value;
-
-    for (let i = 0; i < bucle_number; i++) {
-        agregar_1_cerveza();
-        console.log(cervezas[i]);
-        console.log(cervezas);
-    }
+  for (let i = 0; i < bucle_number; i++) {
+    agregar_1_cerveza();
+  }
+  limpiarForm();
 }
 
-function resetear_cervezas_agregadas() {
-
-    for (let i = cervezas.lenght; i != 0; i--) {
-        restar_1_cerveza();
-    }
-
+function resetear_cervezas() {
+  cervezas = [];
+  cargarTabla();
 }
+
+function filtrar_cervezas() {
+  let switchButton = document.getElementById("mostrar-stock");
+  let cervezas_en_stock = document.querySelectorAll(".en_stock");
+  if (switchButton.checked) {
+    for (let i = 0; i < cervezas_en_stock.length; i++) {
+      cervezas_en_stock[i].classList.add("en_stock", "mostrar");
+    }
+  } else {
+    for (let i = 0; i < cervezas_en_stock.length; i++) {
+      cervezas_en_stock[i].classList.remove("mostrar");
+    }
+  }
+}
+
+document
+  .getElementById("sumar-button")
+  .addEventListener("click", agregar_1_cerveza);
+document
+  .getElementById("restar-button")
+  .addEventListener("click", restar_1_cerveza);
+document
+  .getElementById("sumar-bucle-button")
+  .addEventListener("click", agregar_3_cervezas);
+document
+  .getElementById("reset-button")
+  .addEventListener("click", resetear_cervezas);
+document
+  .getElementById("mostrar-stock")
+  .addEventListener("change", filtrar_cervezas);
